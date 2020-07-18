0c423baa8f7c94744229952f006f8c321e2e3e11
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-18 22:08:02 +0530

    Preloader added , jquery slim removed , full added

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,14 @@
-$(document).click(function(e) {
-	if (!$(e.target).is('.topnav')) {
-    	$('.collapse').collapse('hide');	    
-    }
-});
+
+/* 1. Proloder */
+$(window).on('load', function () {
+	$('.preloader').delay(350).fadeOut('slow');
+	$('body').delay(350).css({
+	  'overflow': 'visible'
+	});
+	$(document).click(function(e) {
+		if (!$(e.target).is('.topnav')) {
+			$('.collapse').collapse('hide');	    
+		}
+	});
+  });
+

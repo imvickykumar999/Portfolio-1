2cf85f8c4492301174c54c68ea27931a4bd2731a
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-18 22:20:47 +0530

    preloader removed

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,10 @@
 
 /* 1. Proloder */
 $(window).on('load', function () {
-	$('.preloader').delay(350).fadeOut('slow');
-	$('body').delay(350).css({
-	  'overflow': 'visible'
-	});
+	// $('.preloader').delay(350).fadeOut('slow');
+	// $('body').delay(350).css({
+	//   'overflow': 'visible'
+	// });
 	$(document).click(function(e) {
 		if (!$(e.target).is('.topnav')) {
 			$('.collapse').collapse('hide');	    

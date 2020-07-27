d097f1aa5cf1c46d2a19ded2dd8c660e8fefe688
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-27 22:45:48 +0530

    Basic Layout

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,14 +1,2 @@
 
-/* 1. Proloder */
-$(window).on('load', function () {
-	// $('.preloader').delay(350).fadeOut('slow');
-	// $('body').delay(350).css({
-	//   'overflow': 'visible'
-	// });
-	$(document).click(function(e) {
-		if (!$(e.target).is('.topnav')) {
-			$('.collapse').collapse('hide');	    
-		}
-	});
-  });
 
